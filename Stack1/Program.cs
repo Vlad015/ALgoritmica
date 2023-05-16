473b4099802ac2f1abf93150151c2f01abd0316f
ASUS <ASUS@DESKTOP-J0OUJQP>
2023-05-17 00:08:19 +0300

    Add project files.

---
+++ Stack1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack A = new Stack();
+            A.Push(1);
+            A.Push(2);
+            A.Push(4);
+            A.Push(5);
+            A.Push(6);
+            A.Push(7);
+            A.Push(3);
+            A.Pop();
+            A.Pop();
+            A.Pop();
+            A.Pop();
+            A.Pop();
+            A.view();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stack1/Stack.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack1
+{
+    internal class Stack
+    {
+        int[] v;
+        int buff;
+        int idx;
+        public Stack() 
+        {
+            buff = 5;
+            idx=0;
+            v = new int[buff];
+        }
+        public void Push(int value)
+        {
+            if(idx==v.Length) 
+            {
+                int[]t= new int[v.Length+buff];
+                for (int i = 0; i < idx; i++)
+                {
+                    t[i] = v[i];
+                }
+                t[idx] = value;
+                idx++;
+                v = t;
+            }
+            else
+            {
+                v[idx] = value;
+                idx++;
+            }
+        }
+        public void Pop() 
+        {
+            idx = v.Length;
+            if((v.Length-idx)==buff)
+            {
+                int[]t= new int[v.Length-buff+1];
+                for (int i = 0; i < idx; i++)
+                {
+                    t[i] = v[i];
+                }
+                idx--;
+                v = t;
+            }
+            else
+            {
+                int[] t= new int[v.Length-1];
+                for (int i = 0; i < idx-1; i++)
+                {
+                    t[i] = v[i];
+                }
+                idx--;
+                v = t;
+            }
+        }
+        public void view()
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                Console.Write(v[i]+" ");
+            }
+        }
+    }
+}
